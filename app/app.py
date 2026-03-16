056352969737333af0b4e645f9998517ec6dc09e
Deepanshu Sharma <deepanshusharma0846@gmail.com>
2026-03-17 01:46:55 +0530

    update-3

--- app/app.py
+++ app/app.py
@@ -5,28 +5,29 @@ from pydantic import BaseModel,Field
 from typing import Optional,Annotated,Literal
 app = FastAPI()
 '''
-Index(['Unnamed: 0', 'trans_date_trans_time', 'cc_num', 'merchant', 'category',
+
+orig Index(['Unnamed: 0', 'trans_date_trans_time', 'cc_num', 'merchant', 'category',
        'amt', 'first', 'last', 'gender', 'street', 'city', 'state', 'zip',
        'lat', 'long', 'city_pop', 'job', 'dob', 'trans_num', 'unix_time',
        'merch_lat', 'merch_long', 'is_fraud'],
       dtype='object')
-
-
+      
+      
 Index(['amt', 'gender', 'city_pop', 'job', 'age', 'hour', 'day_of_week',
-       'month', 'is_weekend', 'cc_num_freq', 'cat_freq', 'merc_freq', 'dist',
-       'amt_vs_avg', 'high_risk_cat'],
+       'month', 'is_weekend', 'cc_num_freq', 'cat_freq', 'dist',
+       'high_risk_cat'],
       dtype='object')
       
 '''
 class CreditCard(BaseModel):
-    trans_date_trans_time: str    # "2024-01-15 14:30:00"
-    dob: str                      # "1990-05-20"
-    category: str                 # "grocery_pos"
-    amt: float                    # 150.75
-    gender: str                   # "Male" / "Female"
-    city_pop: int                 # actual number
-    job: str                      # "Engineer"
-    dist: float
+    trans_date_trans_time: str    # "2024-01-15 14:30:00" 5
+    dob: str                      # "1990-05-20" 1
+    category: str                 # "grocery_pos"--high risk 2
+    amt: float                    # 150.75 1
+    gender: str                   # "Male" / "Female" 1
+    city_pop: int                 # actual number 1
+    job: str                      # "Engineer"1
+    dist: float 1
 
 
 
